0d3bcfcda43e98af372e5d521390538b81608c6a
Lavanya Rapelli <lavanyarapelli555@gmail.com>
2024-07-30 21:12:17 +0530

    All about loops

---
+++ loops.js
@@ -0,0 +1,74 @@
+// let char = "a";
+
+// if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+//     console.log("It is a vowel");
+// } else {
+//     console.log("It is not a vowel");
+// }
+
+
+//  single task for multiple times we should use loops 
+// if we want to console something for 1000 times if we go manual it is so difficult.. so we should use loops 
+
+// let start = 0;
+// while (start <= 5){  // while loops need condition and updation
+//     console.log("hello lavanya")
+//     start += 1;
+// }
+
+// 0 < 5 -- true -- 0
+// 1 < 5 -- true -- 1
+// 2 < 5 -- true -- 2
+// 3 < 5 -- true -- 3
+// 4 < 5 -- true -- 4
+// 5 < 5 -- false -- stop
+
+// let start = 1;
+
+// while(start <= 5){
+//     console.log("Masai School")
+//     start++;
+// }
+
+// let start = 0;
+// while (start <= 10){
+//     console.log(start);
+//     start += 1;
+// }
+
+// let start = 0;
+// while(start <= 10){
+//     if(start % 2 == 0){
+//         console.log(start + " " + "it is even");
+//     }
+//     else{
+//         console.log(start + " " + "it is odd");
+//     }
+//     start += 1;
+// }
+
+// let i = 2;
+// let j = 7;
+// let count = 0;
+// while(i < j ){
+//     if(j % i == 0){
+//         count+=1;
+//     }  
+//     i++;
+// }
+
+// if(count == 0){
+//     console.log("its a prime")
+// }
+// else{
+//     console.log("its not a prime")
+// }
+
+
+
+
+
+
+
+
+
